800958c8c80e617616fa0167618ff058696603ac
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-02 19:41:42 -0700

    created router put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,10 @@
-var express = require ("express");
+var express = require("express");
 var burger = require("../models/burger");
 
 var router = express.Router();
 
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hdbrsObj = {
             burgers: data
         };
@@ -12,13 +12,29 @@ router.get("/", function(req, res) {
         res.sender("index", hdbrsObj);
     });
 
-    router.post("/api/burgers", function(req, res) {
+    router.post("/api/burgers", function (req, res) {
         burger.insertOne(function (
             ["burger_name", "devoured"],
             [req.body.burger_name, req.body.devoured],
-            function(result) {
-                res.json({ id: result.insertID });
+            function (result) {
+                res.json({
+                    id: result.insertID
+                });
             }
         ));
+    });
+
+    router.put("/api/burgers/:id", function (req, res) {
+        var condition = "id = " + req.params.id;
+        console.log("condition", condition);
+        burger.updateOne({
+            devoured: req.body.devoured
+        }, condition, function (result) {
+            if (result, changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        })
     })
 });
