911483da32e36b3797892bbb54748f73013c7b22
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-02 19:34:05 -0700

    created get router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,14 @@
 var express = require ("express");
 var burger = require("../models/burger");
+
+var router = express.Router();
+
+router.ger("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hdbrsObj = {
+            burgers: data
+        };
+        console.log(hdbrsObj);
+        res.sender("index", hdbrsObj);
+    })
+})
