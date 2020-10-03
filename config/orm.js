6670f6d66eaa7e4cd7a7e25ddb67f7189b227a1b
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-02 19:14:37 -0700

    created insert

--- config/orm.js
+++ config/orm.js
@@ -9,6 +9,17 @@ var orm = {
                 throw err;
             }
             cb(res);
-        })
+        });
+    }
+
+    insertOne: function (table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " + table + "(" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
     }
 };
