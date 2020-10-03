7c6dfa753fb7c28b7f05be4d7e3cbbdc4a513958
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-02 19:38:23 -0700

    created router post:

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,12 +3,22 @@ var burger = require("../models/burger");
 
 var router = express.Router();
 
-router.ger("/", function(req, res) {
+router.get("/", function(req, res) {
     burger.selectAll(function(data) {
         var hdbrsObj = {
             burgers: data
         };
         console.log(hdbrsObj);
         res.sender("index", hdbrsObj);
+    });
+
+    router.post("/api/burgers", function(req, res) {
+        burger.insertOne(function (
+            ["burger_name", "devoured"],
+            [req.body.burger_name, req.body.devoured],
+            function(result) {
+                res.json({ id: result.insertID });
+            }
+        ));
     })
-})
+});
