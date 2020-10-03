da8bb2ee7201bdd52e5e68f15d0a5ca8e86e5e80
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-02 19:18:21 -0700

    created deleteOne

--- config/orm.js
+++ config/orm.js
@@ -33,4 +33,15 @@ var orm = {
             cb(res);
         });
     }
+
+    deleteOne: function(table, condition, cb) {
+        var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
 };
